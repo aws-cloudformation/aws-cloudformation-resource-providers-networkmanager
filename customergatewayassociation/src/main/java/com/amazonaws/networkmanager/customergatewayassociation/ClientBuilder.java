ed47e778a6681fa355f483cc3d4152bc341285f3
Chen Zhang <chnzh@amazon.com>
2021-09-13 09:45:27 -0700

    Change the indent & remove file pushed by mistake & fix error in template file

--- customergatewayassociation/src/main/java/com/amazonaws/networkmanager/customergatewayassociation/ClientBuilder.java
+++ customergatewayassociation/src/main/java/com/amazonaws/networkmanager/customergatewayassociation/ClientBuilder.java
@@ -5,17 +5,17 @@ import software.amazon.awssdk.services.networkmanager.NetworkManagerClient;
 import software.amazon.cloudformation.LambdaWrapper;
 
 final class ClientBuilder {
-  static NetworkManagerClient getClient() {
-    String regionString = System.getenv("AWS_REGION");
-    Region region = Region.US_WEST_2;
+    static NetworkManagerClient getClient() {
+        String regionString = System.getenv("AWS_REGION");
+        Region region = Region.US_WEST_2;
 
-    if (regionString != null && (regionString.equals("us-gov-west-1") || regionString.equals("us-gov-east-1"))) {
-      region = Region.US_GOV_WEST_1;
+        if (regionString != null && (regionString.equals("us-gov-west-1") || regionString.equals("us-gov-east-1"))) {
+            region = Region.US_GOV_WEST_1;
+        }
+
+        return NetworkManagerClient.builder()
+                .httpClient(LambdaWrapper.HTTP_CLIENT)
+                .region(region)
+                .build();
     }
-
-    return NetworkManagerClient.builder()
-            .httpClient(LambdaWrapper.HTTP_CLIENT)
-            .region(region)
-            .build();
-  }
 }
